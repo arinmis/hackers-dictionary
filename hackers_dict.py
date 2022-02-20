85d618de84bd86bdaf0e3027c94966d9b00e0586
Arinmis <mustafa_arinmis@outlook.com>
2022-02-21 01:30:47 +0300

    Add command line usage

--- hackers_dict.py
+++ hackers_dict.py
@@ -3,8 +3,13 @@
 import requests
 import sys
 
-DICT_API = "http://127.0.0.1:8000/"
-word = sys.argv[1]
+DICT_API = "http://127.0.0.1:8000/api/dictionary/"
+try: 
+    word = sys.argv[1]
+except:
+    print("USAGE: means <searched-word>")
+    sys.exit(1)
+
 request = requests.get(DICT_API + word) 
 
 if not request.json()["isFound"]:
