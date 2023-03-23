19366291ae29ef0ad4f6be2e3f34288786b8d8e6
arinmis <mustafa_arinmis@outlook.com>
2023-03-23 18:08:50 +0300

    Request to dictonary API

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+const axios = require('axios');
 
-const word =  argv.word || argv._[0] 
+const word =  (argv.word || argv._[0]).trim()
+axios.defaults.baseURL = "https://dictionary-api11.herokuapp.com";
 
+axios.get(`/api/dictionary/${word}`)
+  .then(function (response) {
+    // handle success
+    console.log(response.data.desc);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(`Sorry... '${word}' is not found`);
+})
 
